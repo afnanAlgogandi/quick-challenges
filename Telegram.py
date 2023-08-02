ffb5b12edeed3d82f497c557efc38aee986a3ab3
Mohammed Adel Alhydry <50909741+alhydrym7@users.noreply.github.com>
2023-08-02 13:07:11 +0300

    Update Telegram.py

--- Telegram.py
+++ Telegram.py
@@ -1,7 +1,13 @@
-# import pywhatkit as ws
 
-t ="    hello, i am Mohammed Adel"
-o = t.strip()
-print(t)
-print(o)
-# ws.sendwhatmsg("","hi",0,0)
+import requests
+token = '6052800085:AAEjNPlYLKTW8FNA6Rgb91BMHfiQS14zEN4'
+groupId = 'fullizaag'
+msg = 'صعبه صعبه ذي ههههههههههه'
+def sendMsg(message):
+    url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id=@{groupId}&text={message}'
+    res = requests.get(url)
+    if res.status_code==200:
+        print('Successfully sent')
+    else:
+        print('ERROR: Could not send Message')
+sendMsg(msg)
